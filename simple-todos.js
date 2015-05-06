99c0a57287134b91d0b6b421166a52f39309b39b
Bishin Ho <bishin.ho@gmail.com>
2015-05-06 22:31:26 +1000

    first commit

---
+++ simple-todos.js
@@ -0,0 +1,15 @@
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" },
+    ]
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
